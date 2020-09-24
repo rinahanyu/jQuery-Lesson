717a20663ccb95aaa521b96eb879e0bfcc21fa6e
rinahanyu <gravity0112@gmail.com>
2020-09-24 17:04:50 +0900

    [Finish]６章イベント

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 $(function(){
-  $('.box1').slideDown(function(){
-    $('.box1').css({
-      'height': '100px',
-      'width': '200px',
-      'background-color': '#0000FF'
-    }).slideUp();
+  $('.box1').on('click', function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
   });
 });
